1c1fc38bc14d52839e257a1802c5a3b50cb1500f
Ladislav Smola <lsmola@redhat.com>
2017-05-19 10:41:46 +0200

    WIP

--- app/models/manager_refresh/inventory_collection_default/cloud_manager.rb
+++ app/models/manager_refresh/inventory_collection_default/cloud_manager.rb
@@ -204,11 +204,12 @@ class ManagerRefresh::InventoryCollectionDefault::CloudManager < ManagerRefresh:
                                  .select([:id, :ancestry])
                                  .where(:id => stacks_parents.values).find_each.index_by(&:id)
 
-        model_class
-          .select([:id, :ancestry])
-          .where(:id => stacks_parents.keys).find_each do |stack|
-          parent = stacks_parents_indexed[stacks_parents[stack.id]]
-          stack.update_attribute(:parent, parent)
+        ActiveRecord::Base.transaction do
+          model_class.select([:id, :ancestry])
+                     .where(:id => stacks_parents.keys).find_each do |stack|
+            parent = stacks_parents_indexed[stacks_parents[stack.id]]
+            stack.update_attribute(:parent, parent)
+          end
         end
       end
 
@@ -241,25 +242,30 @@ class ManagerRefresh::InventoryCollectionDefault::CloudManager < ManagerRefresh:
           end
         end
 
-        # associate parent templates to child instances
-        parent_miq_templates = miq_templates_inventory_collection.model_class
-                                                                 .select([:id])
-                                                                 .where(:id => vms_genealogy_parents.values).find_each.index_by(&:id)
-        vms_inventory_collection.model_class
-                                .select([:id])
-                                .where(:id => vms_genealogy_parents.keys).find_each do |vm|
-          parent = parent_miq_templates[vms_genealogy_parents[vm.id]]
-          vm.with_relationship_type('genealogy') { vm.parent = parent }
+        ActiveRecord::Base.transaction do
+          # associate parent templates to child instances
+          parent_miq_templates = miq_templates_inventory_collection.model_class
+                                                                   .select([:id])
+                                                                   .where(:id => vms_genealogy_parents.values).find_each.index_by(&:id)
+          vms_inventory_collection.model_class
+                                  .select([:id])
+                                  .where(:id => vms_genealogy_parents.keys).find_each do |vm|
+            parent = parent_miq_templates[vms_genealogy_parents[vm.id]]
+            vm.with_relationship_type('genealogy') { vm.parent = parent }
+          end
         end
-        # associate parent instances to child templates
-        parent_vms = vms_inventory_collection.model_class
-                                             .select([:id])
-                                             .where(:id => miq_template_genealogy_parents.values).find_each.index_by(&:id)
-        miq_templates_inventory_collection.model_class
-                                          .select([:id])
-                                          .where(:id => miq_template_genealogy_parents.keys).find_each do |miq_template|
-          parent = parent_vms[miq_template_genealogy_parents[miq_template.id]]
-          miq_template.with_relationship_type('genealogy') { miq_template.parent = parent }
+
+        ActiveRecord::Base.transaction do
+          # associate parent instances to child templates
+          parent_vms = vms_inventory_collection.model_class
+                                               .select([:id])
+                                               .where(:id => miq_template_genealogy_parents.values).find_each.index_by(&:id)
+          miq_templates_inventory_collection.model_class
+                                            .select([:id])
+                                            .where(:id => miq_template_genealogy_parents.keys).find_each do |miq_template|
+            parent = parent_vms[miq_template_genealogy_parents[miq_template.id]]
+            miq_template.with_relationship_type('genealogy') { miq_template.parent = parent }
+          end
         end
       end
 
